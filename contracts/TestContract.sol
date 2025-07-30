5f74f42bd28c5df78fe9b62f7dcd8e76ea3c72af
nisarg-shah-222 <nisarg25008@gmail.com>
2025-07-30 22:16:58 +0530

    adding sample storage

--- contracts/TestContract.sol
+++ contracts/TestContract.sol
@@ -1,3 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-contract TestContract {}
+
+contract SimpleStorage {
+    uint256 public storedValue;
+
+    function set(uint256 _value) external {
+        storedValue = _value;
+    }
+
+    function get() external view returns (uint256) {
+        return storedValue;
+    }
+}
