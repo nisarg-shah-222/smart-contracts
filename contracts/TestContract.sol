b5f13ba5c82aa8298d877feac874219684a4292c
nisarg-shah-222 <nisarg25008@gmail.com>
2025-07-30 22:43:55 +0530

    adding changes

--- contracts/TestContract.sol
+++ contracts/TestContract.sol
@@ -1,14 +1,3 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
-contract SimpleStorage {
-    uint256 public storedValue;
-
-    function set(uint256 _value) external {
-        storedValue = _value;
-    }
-
-    function get() external view returns (uint256) {
-        return storedValue;
-    }
-}
+contract TestContract {}
